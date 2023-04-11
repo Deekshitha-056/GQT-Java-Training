614bebfb4f8a48530d12d6410201dc70d65af03a
DEEKSHITHA S <DEEKSHITHA S@LAPTOP-TTRE143E>
2023-04-11 07:35:28 +0530

    ARRAYS AND STRINGS CODE

---
+++ src/com/gqt/arraysproj/pkj1/ArrayCode4.java
@@ -0,0 +1,15 @@
+package com.gqt.arraysproj.pkj1;
+/*	(i)	 (j)	   (k)		   (l)
+ * 	Uni	school    class		students
+ * 	 0	  0			0			5
+ * 					1			5
+ * 		  1			0			5
+ * 					1			5
+ *   1	  0			0			5
+ *  				1			5
+ *  	  1			0			5
+ *  				1			5
+ */
+public class ArrayCode4 {
+
+}
